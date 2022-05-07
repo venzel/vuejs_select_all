8fefc1a55fc474e9d54a4f81578e2cb56fa5d2fd
venzel <eneas.eng@yahoo.com>
2022-05-06 22:09:47 -0300

    feat: init

--- README.md
+++ README.md
@@ -1,19 +1,1 @@
 # vuejs_select_all
-
-## Project setup
-```
-yarn install
-```
-
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
-
-### Compiles and minifies for production
-```
-yarn build
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app>
+        <v-main>
+            <div>{{ selecteds }}</div>
+            <v-container>
+                <v-select
+                    :items="users"
+                    item-text="name"
+                    v-model="selecteds"
+                    label="Selecione os usuários"
+                    rounded
+                    multiple>
+                    <template v-slot:prepend-item>
+                        <v-list-item ripple @mousedown.prevent @click="toggle">
+                            <v-list-item-action>
+                                <v-icon
+                                    :color="
+                                        selecteds.length > 0 ? 'indigo darken-4' : ''
+                                    ">
+                                    {{ icon }}
+                                </v-icon>
+                            </v-list-item-action>
+
+                            <v-list-item-content>
+                                <v-list-item-title> Selecionar todos </v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </template>
+                </v-select>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    export default {
+        name: 'App',
+        data() {
+            return {
+                users: [
+                    { name: 'Tiago', age: 22 },
+                    { name: 'Cintia', age: 24 },
+                    { name: 'Liz', age: 23 },
+                    { name: 'Everton', age: 26 },
+                    { name: 'Eline', age: 20 }
+                ],
+                selecteds: []
+            };
+        },
+        computed: {
+            likesAllUser() {
+                return this.selecteds.length === this.users.length;
+            },
+            likesSomeUser() {
+                return this.selecteds.length > 0 && !this.likesAllUser;
+            },
+            icon() {
+                if (this.likesAllUser) return 'mdi-close-box';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+                if (this.likesSomeUser) return 'mdi-minus-box';
+
+                return 'mdi-checkbox-blank-outline';
+            }
+        },
+        methods: {
+            toggle() {
+                this.$nextTick(() => {
+                    if (this.likesAllUser) {
+                        this.selecteds = [];
+                    } else {
+                        this.selecteds = this.users.slice();
+                    }
+                });
+            }
+        }
+    };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
